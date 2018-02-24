2154f10ef717470bef3c0a92d7036e5ad1c514d4
mikymod <mikymod@gmail.com>
2018-02-24 23:56:04 +0100

    Add MainMenu

---
+++ Assets/MainMenuUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void TaskResumeButton()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void TaskRestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1.0f;
+    }
+
+    public void TaskQuitButton()
+    {
+        Time.timeScale = 0.0f;
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Scripts/OpenMenuUI.cs
+++ Assets/Scripts/OpenMenuUI.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class OpenMenuUI : MonoBehaviour
 {
+    public GameObject mainMenu;
+
     public void TaskOpenMenu()
     {
-        Debug.Log("Open menu");
+        Debug.Log("Open Main Menu");
+        mainMenu.SetActive(true);
+
+        // Pause game
+        Time.timeScale = 0f;
     }
 }
