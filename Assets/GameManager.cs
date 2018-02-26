4617d7b70d2ff0b0e77bc8a3d69084a5eb48b046
mikymod <mikymod@gmail.com>
2018-02-26 17:47:24 +0100

    Add GameOverUI

---
+++ Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool GameIsOver = false;
+
+    public GameObject gameOverUI;
+
+    void Start()
+    {
+        GameIsOver = false;
+    }
+
+    void Update()
+    {
+        if (GameIsOver)
+            return;
+
+        if (Player.Health <= 0)
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        Time.timeScale = 0f;
+        GameIsOver = true;
+        gameOverUI.SetActive(true);
+    }
+}
---
+++ Assets/GameOverUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverUI : MonoBehaviour
+{
+    public Text waveText;
+
+    void Awake()
+    {
+        waveText.text = "Wave: " + Player.Round;
+    }
+
+    public void TaskRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1.0f;
+    }
+
+    public void TaskQuit()
+    {
+        Application.Quit();
+    }
+
+}
