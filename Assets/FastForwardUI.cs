8df23cdbfbce8fb813d2ce388f6d3ad7bb39a31c
mikymod <mikymod@gmail.com>
2018-02-26 18:46:01 +0100

    Add Fast Forward

---
+++ Assets/FastForwardUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FastForwardUI : MonoBehaviour
+{
+    private Toggle forwardToggle;
+
+    void Awake()
+    {
+        forwardToggle = gameObject.GetComponent<Toggle>();
+    }
+
+    public void TaskPerformFastForward()
+    {
+        if (forwardToggle.isOn)
+            Time.timeScale = 2f;
+        else
+            Time.timeScale = 1f;
+    }
+}
